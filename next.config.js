f1da15271fd6cae5530308a3ad50629b4d5f587f
anisa07 <askany07@gmail.com>
2022-06-05 23:56:11 +0200

    fix build directory

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  distDir: 'out',
   env: {
     URL_API: process.env.URL_API,
     FEEDBACK_USER_KEY: process.env.FEEDBACK_USER_KEY,
